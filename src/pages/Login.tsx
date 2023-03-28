57aa7302f12ec0fb3f5741cb11decc6816d924d9
jjh9329 <109144598+jjh9329@users.noreply.github.com>
2023-03-28 19:43:03 +0900

    feat(util,style):  dowonload
    Co-authored-by: Ropung <Ropung@users.noreply.github.com>

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,12 +1,22 @@
 const Login = () => {
   return (
-    <div>
-      <h1>dkssud</h1>
-      <form action="/login" method="post">
-        <input className="border border-gray-400" type="text" name="username" />
-        <input className="border border-gray-400" type="password" name="password" />
-      </form>
-    </div>
+    <section className="w-full min-h-screen bg-red-50">
+      <h1>화면 연습용</h1>
+      <div className="w-full h-28 flex flex-col gap-2 bg-blue-100">
+        <form action="/login" method="post">
+          <input className="border border-blue-400" type="text" name="username" />
+          <input className="border border-gray-400" type="password" name="password" />
+        </form>
+      </div>
+      <div>
+        <div className="w-full h-28 flex flex-col gap-2 bg-green-200">
+          <form action="/login" method="post">
+            <input className="border border-blue-400" type="text" name="username" />
+            <input className="border border-gray-400" type="password" name="password" />
+          </form>
+        </div>
+      </div>
+    </section>
   )
 }
 
---
+++ src/utils/path/routes/index.ts
@@ -0,0 +1,17 @@
+const Path = {
+  HOME: "/",
+  LOGIN: "/login",
+  USERS: "/users/manage",
+  GRADE: "/garde/manage",
+  WRITER_APPROVE: "/write/approve",
+  WRITER_MANAGE: "/write/manage",
+  NOVEL_MANAGE: "/novel/manage",
+  ANNOUNCE_MANAGE: "/announc/manage",
+  BANNER_MANAGE: "/banner/manage",
+  COMMENT_MANAGE: "/comment/manage",
+  COMPLAIN_MANAGE: "/complain/manage",
+};
+
+Object.freeze(Path);
+
+export default Path;
